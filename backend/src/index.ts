06b0721bad2ec7d04547a551ebe8db5f8fa183d4
Bharat Gawar <cbhrat50@gmail.com>
2025-03-20 15:18:22 +0530

    expose notes and folders routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./utils/connectDb";
 import userRoutes from "./routes/userRoute";
+import folderRoutes from "./routes/folderRoutes";
+import noteRoutes from "./routes/noteRoute";
 import { errorHandler } from "./middlewares/errorHandler";
 
 dotenv.config();
@@ -17,6 +19,8 @@ app.use(express.json());
 
 // Routes
 app.use("/api/users", userRoutes);
+app.use("/api/folders", folderRoutes);
+app.use("/api/notes", noteRoutes);
 
 // Health check route
 app.get("/", (req: Request, res: Response) => {
